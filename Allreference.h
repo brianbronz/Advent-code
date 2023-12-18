c76357b544392425404271987fdda259677fabfa
brianbronz <55786030+brianbronz@users.noreply.github.com>
2023-12-18 08:53:59 +0100

    Add files via upload

---
+++ Allreference.h
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <dirent.h>
+#include <ostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctype.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <map>
+
+using namespace std;
